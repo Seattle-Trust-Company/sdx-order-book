6ddab49d2a21016b93e314bd58d9395017ccaeb9
johnedquinn <johnedquinn@gmail.com>
2021-04-16 15:26:30 -0400

    Initialize currencies

---
+++ OrderBook/Currencies.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+def getCurrencies():
+  return { 'msg': 'To be developed.' }, 404
--- server.py
+++ server.py
@@ -13,6 +13,7 @@ import json
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from OrderBook import Products
+from OrderBook import Currencies
 
 
 ### Globals
@@ -30,23 +31,40 @@ def getProducts():
   products, code = Products.getProducts()
   return jsonify(products), code
 
-@app.route('/product/<pid>', methods=['GET'])
+@app.route('/products/<pid>', methods=['GET'])
 def getProduct(pid):
   product, code = Products.getProduct(pid)
   return jsonify(product), code
 
-@app.route('/product/<pid>/book', methods=['GET'])
+@app.route('/products/<pid>/book', methods=['GET'])
 def getProductBook(pid):
   book, code = Products.getProductBook(pid)
   return jsonify(book), code
 
-@app.route('/product/<pid>/ticker', methods=['GET'])
+@app.route('/products/<pid>/ticker', methods=['GET'])
 def getProductTicker(pid):
   tick, code = Products.getProductTicker(pid)
   return jsonify(tick), code
 
+@app.route('/products/<pid>/trades', methods=['GET'])
+def getProductTrades(pid):
+  trades, code = Products.getProductTrades(pid)
+  return jsonify(trades), code
 
+@app.route('/products/<pid>/candles', methods=['GET'])
+def getProductCandles(pid):
+  candles, code = Products.getProductCandles(pid)
+  return jsonify(candles), code
 
+@app.route('/products/<pid>/stats', methods=['GET'])
+def getProductStats(pid):
+  stats, code = Products.getProductStats(pid)
+  return jsonify(stats), code
+
+@app.route('/currencies', methods=['GET'])
+def getProducts():
+  currencies, code = Currencies.getCurrencies()
+  return jsonify(currencies), code
 
 ### Main Execution
 
